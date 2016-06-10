684a4cb6a681f9d574d4419c71c75436c50204bf
M1008643 <M1008643@B2ML17443>
2016-06-10 21:06:41 +0530

    version 0.1

---
+++ HappyServerSide/Happy/HappyCommon/HappyAlertRepository.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using Microsoft.WindowsAzure.Storage.Table.Queryable;
+using Microsoft.WindowsAzure.Storage.Analytics;
+using Microsoft.WindowsAzure.Storage.Table.Protocol;
+using System.Reflection;
+
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Table.DataServices;
+using Microsoft.WindowsAzure;
+using System.Data.Services.Client;
+using System.Reflection;
+
+namespace HappyCommon
+{
+    public class HappyAlertsRepository : IRepository
+    {
+        readonly string _happyStorageConnectionString;
+        readonly CloudStorageAccount _happyStorageAccount;
+        CloudTableClient _tableClient;
+        CloudTable _table;
+        TableOperation _insertOperation;
+        TableOperation _deleteOperation;
+        TableOperation _replaceOperation;
+
+        public const string DEFAULT_TABLE_NAME = "HappyAlerts";
+        public const string DEFAULT_PART_KEY = "HAPPY";
+        public const string DEFAULT_STR_VALUE = "EMPTY";
+
+        private readonly IConfigManager _configManager;
+
+        bool _disposed = false;
+        string _partKey = string.Empty;
+
+        public HappyAlertsRepository(string tableName, string partKey)
+            : this(new HappyConfigManager(), tableName, partKey)
+        {
+        }
+
+
+        public HappyAlertsRepository(IConfigManager configMgr,
+                                     string tableName, string partKey)
+        {
+            _configManager = configMgr;
+
+            _happyStorageConnectionString = _configManager.GetConfigurationSetting("AzureStorage.ConnectionString");
+            _happyStorageAccount = CloudStorageAccount.Parse(_happyStorageConnectionString);
+            _tableClient = _happyStorageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            _table = _tableClient.GetTableReference(tableName);
+            _partKey = partKey;
+        }
+
+        public void InsertHappyAlertDetails(HappyAlert device)
+        {
+            TableResult result;
+            // Create the table if it doesn't exist.
+            _table.CreateIfNotExists();
+            try
+            {
+                // Create the TableOperation object that inserts the customer entity.
+                _insertOperation = TableOperation.Insert(device);
+
+                // Execute the insert operation.
+                result = _table.Execute(_insertOperation);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        public List<HappyAlert> GetAll(string rowKey)
+        {
+            List<HappyAlert> dEntList = new List<HappyAlert>();
+            HappyAlert dEnt = null;
+
+            TableQuery<HappyAlert> rangeQuery = new TableQuery<HappyAlert>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, _partKey),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, rowKey)
+                 )
+                );
+
+            foreach (HappyAlert e in _table.ExecuteQuery(rangeQuery))
+            {
+                char[] delim = { ',' };
+
+                dEnt = new HappyAlert();
+                dEnt.RowKey = (e.RowKey == null) ? DEFAULT_STR_VALUE : e.RowKey;
+                //dEnt.Id = (e.RowKey == null) ? "EMPTY" : e.RowKey;
+                dEnt.Name = (e.Name == null) ? DEFAULT_STR_VALUE : e.Name;
+                dEnt.AlertMessage = (e.AlertMessage == null) ? DEFAULT_STR_VALUE : e.AlertMessage;
+                if (dEnt.roles != null)
+                {
+                    dEnt.AllowedRoles = dEnt.roles.Split(delim);
+                }
+                dEnt.roles = (e.roles == null) ? DEFAULT_STR_VALUE : e.roles;
+                dEnt.Description = (e.Description == null) ? DEFAULT_STR_VALUE : e.Description;
+
+
+                dEntList.Add(dEnt);
+            }
+
+            return dEntList;
+        }
+
+
+        public bool IsExist(string rowKey)
+        {
+            bool retVal = false;
+
+            TableQuery<HappyAlert> rangeQuery = new TableQuery<HappyAlert>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, _partKey),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, rowKey)
+                 )
+                );
+
+            foreach (HappyAlert e in _table.ExecuteQuery(rangeQuery))
+            {
+                retVal = true;
+                break;
+            }
+
+            return retVal;
+        }
+
+
+        public List<HappyAlert> GetAll()
+        {
+            List<HappyAlert> dEntList = new List<HappyAlert>();
+            HappyAlert dEnt;
+
+            TableQuery<HappyAlert> rangeQuery = new TableQuery<HappyAlert>().Where(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, _partKey)
+                );
+
+            foreach (HappyAlert e in _table.ExecuteQuery(rangeQuery))
+            {
+                char[] delim = { ',' };
+
+                dEnt = new HappyAlert();
+                dEnt.RowKey = (e.RowKey == null) ? DEFAULT_STR_VALUE : e.RowKey;
+                //dEnt.Id = (e.RowKey == null) ? "EMPTY" : e.RowKey;
+                dEnt.Name = (e.Name == null) ? DEFAULT_STR_VALUE : e.Name;
+                dEnt.AlertMessage = (e.AlertMessage == null) ? DEFAULT_STR_VALUE : e.AlertMessage;
+                if (e.roles != null)
+                {
+                    e.roles = e.roles.Replace(" ", "");
+                    dEnt.AllowedRoles = e.roles.Split(delim);
+
+                }
+                dEnt.roles = (e.roles == null) ? DEFAULT_STR_VALUE : e.roles;
+                dEnt.Description = (e.Description == null) ? DEFAULT_STR_VALUE : e.Description;
+
+
+                dEntList.Add(dEnt);
+
+            }
+
+            return dEntList;
+        }
+
+
+        public List<HappyAlert> GetLatest()
+        {
+            List<HappyAlert> dEntList = new List<HappyAlert>();
+            HappyAlert dEnt;
+
+            TableQuery<HappyAlert> rangeQuery = new TableQuery<HappyAlert>().Where(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, _partKey)
+                );
+
+            foreach (HappyAlert e in _table.ExecuteQuery(rangeQuery))
+            {
+                char[] delim = { ',' };
+
+                dEnt = new HappyAlert();
+                dEnt.RowKey = (e.RowKey == null) ? DEFAULT_STR_VALUE : e.RowKey;
+                //dEnt.Id = (e.RowKey == null) ? "EMPTY" : e.RowKey;
+                dEnt.Name = (e.Name == null) ? DEFAULT_STR_VALUE : e.Name;
+                dEnt.AlertMessage = (e.AlertMessage == null) ? DEFAULT_STR_VALUE : e.AlertMessage;
+                if (e.roles != null)
+                {
+                    e.roles = e.roles.Replace(" ", "");
+                    dEnt.AllowedRoles = e.roles.Split(delim);
+
+                }
+                dEnt.roles = (e.roles == null) ? DEFAULT_STR_VALUE : e.roles;
+                dEnt.Description = (e.Description == null) ? DEFAULT_STR_VALUE : e.Description;
+
+
+                dEntList.Add(dEnt);
+
+            }
+
+            return dEntList;
+        }
+
+        public List<HappyAlert> GetAllForRole(string role)
+        {
+            List<HappyAlert> dEntList = new List<HappyAlert>();
+            HappyAlert dEnt;
+
+            TableQuery<HappyAlert> rangeQuery = new TableQuery<HappyAlert>().Where(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, _partKey)
+                );
+
+            foreach (HappyAlert e in _table.ExecuteQuery(rangeQuery))
+            {
+                char[] delim = { ',' };
+
+                dEnt = new HappyAlert();
+                dEnt.RowKey = (e.RowKey == null) ? DEFAULT_STR_VALUE : e.RowKey;
+                //dEnt.Id = (e.RowKey == null) ? "EMPTY" : e.RowKey;
+                dEnt.Name = (e.Name == null) ? DEFAULT_STR_VALUE : e.Name;
+                dEnt.AlertMessage = (e.AlertMessage == null) ? DEFAULT_STR_VALUE : e.AlertMessage;
+                if (e.roles != null)
+                {
+
+                    e.roles = e.roles.Replace(" ", "");
+                    dEnt.AllowedRoles = e.roles.Split(delim);
+
+                }
+                dEnt.roles = (e.roles == null) ? DEFAULT_STR_VALUE : e.roles;
+                dEnt.Description = (e.Description == null) ? DEFAULT_STR_VALUE : e.Description;
+
+                if (dEnt.roles.ToLower().IndexOf(role.ToLower()) >= 0)
+                {
+                    dEntList.Add(dEnt);
+                }
+
+            }
+
+            return dEntList;
+        }
+
+
+    }
+
+    public class HappyAlert : TableEntity
+    {
+        public string DEFAULT_PARTKEY = "HAPPY";
+
+        public static long runningNumber = 0;
+        public HappyAlert()
+        {
+
+        }
+        public HappyAlert(string name, string amsg, string desc)
+        {
+
+            Name = name;
+            AlertMessage = amsg;
+            Description = desc;
+            PartitionKey = DEFAULT_PARTKEY;
+            Sno = runningNumber++;
+            RowKey = name + runningNumber.ToString();
+        }
+
+        public long Sno
+        {
+            get;set;
+        }
+
+
+        public string Name
+        {
+            get; set;
+        }
+        public string AlertMessage
+        {
+            get; set;
+        }
+
+        public string Description
+        {
+            get; set;
+        }
+
+        public string roles
+        {
+            get; set;
+        }
+        public string[] AllowedRoles
+        {
+            get; set;
+        }
+
+        public string htmlString
+        {
+            get; set;
+        }
+
+    }
+}
---
+++ HappyServerSide/Happy/HappyService/Controllers/AlertsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Converters;
+
+using HappyCommon;
+
+namespace HappyService.Controllers
+{
+    public class AlertsController : ApiController
+    {
+        public static string MsgName = "Empty";
+        public static string MsgContent = "Empty";
+
+        [HttpGet]
+        public async Task<JsonResult<List<HappyAlert>>> GetAll()
+        {
+            List<HappyAlert> appList = new List<HappyAlert>();
+
+            HappyAlert ha = new HappyAlert(MsgName, MsgContent, "Alert Message");
+            appList.Add(ha);
+            //HappyAlertsRepository mRepo = new HappyAlertsRepository(HappyAlertsRepository.DEFAULT_TABLE_NAME, HappyAlertsRepository.DEFAULT_PART_KEY);
+            //appList = mRepo.GetAll();
+
+            return Json<List<HappyAlert>>(appList);
+        }
+
+
+        [HttpGet]
+        public async Task<JsonResult<List<HappyAlert>>> GetAllForRole(string role)
+        {
+            List<HappyAlert> appList = new List<HappyAlert>();
+
+            HappyAlert ha = new HappyAlert(MsgName, MsgContent, "Alert Message");
+            appList.Add(ha);
+            //HappyAlertsRepository mRepo = new HappyAlertsRepository(HappyAlertsRepository.DEFAULT_TABLE_NAME, HappyAlertsRepository.DEFAULT_PART_KEY);
+            //appList = mRepo.GetAll();
+
+
+            return Json<List<HappyAlert>>(appList);
+        }
+
+
+        [HttpPost]
+        public async Task<JsonResult<HappyAlert>> InsertHappyAlert(HappyAlert mApp)
+        {
+            HappyAlertsRepository mRepo = new HappyAlertsRepository(HappyAlertsRepository.DEFAULT_TABLE_NAME, HappyAlertsRepository.DEFAULT_PART_KEY);
+            HappyAlert HappyAlert;
+            char[] delim = { ',' };
+            if (mApp.Name != null && mApp.AlertMessage != null)
+            {
+                //HappyAlert = new HappyAlert(mApp.Name, mApp.AlertMessage, mApp.Description);
+                //if (mApp.roles != null)
+                //{
+                //    HappyAlert.roles = mApp.roles;
+                //    HappyAlert.AllowedRoles = mApp.roles.Split(delim);
+                //}
+                //mRepo.InsertHappyAlertDetails(HappyAlert);
+                //mApp = HappyAlert;
+                MsgName = mApp.Name;
+                MsgContent = mApp.AlertMessage;
+            }
+
+            return Json<HappyAlert>(mApp);
+        }
+
+        [HttpGet]
+        public async Task<JsonResult<HappyAlert>> Insert(string Name, string Message, string Desc)
+        {
+            HappyAlertsRepository mRepo = new HappyAlertsRepository(HappyAlertsRepository.DEFAULT_TABLE_NAME, HappyAlertsRepository.DEFAULT_PART_KEY);
+            HappyAlert happyAlert = new HappyAlert();
+            char[] delim = { ',' };
+            if (Name != null && Message != null)
+            {
+                happyAlert = new HappyAlert(Name, Message, Desc);
+
+                MsgName = Name;
+                MsgContent = Message;
+            }
+
+            return Json<HappyAlert>(happyAlert);
+        }
+
+    }
+
+}
